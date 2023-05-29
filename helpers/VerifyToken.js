7eec67dcbe28aae13abf1d277e098c11828008fa
carlosthrasher <carloshcpereira@outlook.com.br>
2023-05-28 22:26:04 -0300

    atualizado respostas para usuario

--- helpers/VerifyToken.js
+++ helpers/VerifyToken.js
@@ -3,13 +3,13 @@ require('dotenv/config');
 
 module.exports = function(req, res, next) {
     const token = req.header('auth-token');
-    if (!token) return res.status(401).send('Access Denied');
+    if (!token) return res.status(401).send('Acesso negado');
 
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
         req.user = verified;
         next();
     } catch (err) {
-        res.status(400).send('Invalid Token');
+        res.status(400).send('Token Inválido');
     }
 };
